88175280f6e175e3e06fe2492f1757cac2a9d1aa
Asma Makhdoom <asmamakhdoom@nbp-130-15.nbp.ruw.rutgers.edu>
2021-05-04 18:31:34 -0400

    alot of work for improved

--- improved.py
+++ improved.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 from statistics import mode
 import sys
+from basic import greyScaleImg
 
 image = cv2.imread('palm2.webp')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -38,7 +39,7 @@ def lossFunc(out2, image, middlePixInd):
     loss = (out2[0] - image[x][y][0])**2 + (out2[1] - image[x][y][1])**2 + (out2[2] - image[x][y][2])**2
     return loss
 
-def updateWeights(out1W, out2W, out1, out2, image, middlePixInd):
+def updateWeights(out1W, out2W, out1, out2, image, middlePixInd, patch):
     x,y = middlePixInd
     middlePix = image[x][y]
 
@@ -53,11 +54,22 @@ def updateWeights(out1W, out2W, out1, out2, image, middlePixInd):
     newWeight1 = np.dot(dotProd,np.transpose(out1))
     
     # Calculate weight2
-    
-    
+    # Calculate the deriv term by doing a sum
+    derivLoss2 = np.array([0.0]**6)
+    for i in range(3):
+        derivLoss3 = 2*(out2[i] - np.array([middlePix])[i])
+        sigPrime2 = derivSigmoid(np.dot(out2W[i],out1))
+        prod = derivLoss3 * sigPrime2
+        derivLoss2 += (prod * out2W[i])
+    derivLoss2 = np.diagflat(derivLoss2)
+    sigPrime3 = derivSigmoid(np.dot(out1W, np.transpose(out1)))
+    dotProd2 = np.dot(sigPrime3,patch)
+
+    newWeight2 = np.dot(derivLoss2,dotProd2)
+
     return newWeight1, newWeight2
 
-def train(leftGrey, image, ):
+def train(leftGrey, image):
     leftPatches = []
     for x in range(1,len(leftGrey)-1):
         for y in range(1,len(leftGrey[0])-1):
@@ -70,10 +82,14 @@ def train(leftGrey, image, ):
     # pick  random patch and plug in patch into the model
     cntr = 0
     prevLoss = sys.maxsize
+    alpha = 10**-8
     while(true):
         x = random.randint(0,len(leftPatches)-1)
         out1, out2 = getOutputFromModel(out1W, out2W, leftPatches[x][0])
         loss = lossFunc(out2, image, leftPatches[x][1])
+        newWeight1, newWeight2 = updateWeights(out1W, out2W, out1, out2, image, leftPatches[x][1], leftPatches[x][0])
+        out1W -= alpha*newWeight1
+        out2W -= alpha*newWeight2
         cntr += 1
         if cntr == 1000:
             change = abs(prevLoss - loss) / prevLoss
@@ -83,11 +99,40 @@ def train(leftGrey, image, ):
                 continue
             else:
                 break
+    return out1W, out2W
         
+def recolorRight(image, greyRight, out1W, out2W):
+    rightToColor = image[:,half:]
+    for i in range(1,len(rightGrey)-1):
+        for j in range(1,len(rightGrey[0])-1):
+            # get 3x3 gray patch from right side
+            rightPatch = np.array([ [rightGrey[i-1][j-1],rightGrey[i][j-1],rightGrey[i+1][j-1]],
+                                    [rightGrey[i-1][j],rightGrey[i][j],rightGrey[i+1][j]],
+                                    [rightGrey[i-1][j+1],rightGrey[i][j+1],rightGrey[i+1][j+1]]])
+            out1, out2 = getOutputFromModel(out1W, out2W, rightPatch)
+            newR, newG, newB = out2
 
-        
+            rightToColor[i][j][0] = newR*255
+            rightToColor[i][j][1] = newG*255
+            rightToColor[i][j][2] = newB*255
+    return rightToColor
 
 
-def recolorRight():
+def improvedAgent(image):
+    # Convert image to grey scale
+    greyImg = greyScaleImg(deepcopy(image))
+    half = int(len(image[0])/2)
+    leftColoredImg = img[:,:half]
+    # rightColorImg = img[:,half:]
+    leftGrey = greyImg[:,:half]
+    rightGrey = greyImg[:,half:]
+    out1W, out2W = train(leftGrey, image)
+    rightColoredImg = recolorRight(image, rightGrey, out1W, out2W)
+    # Combine the recolored right and original left image
+    new = []
+    for i in range(0, len(leftColoredImg)):
+        new.append(list(leftColoredImg[i])+list(rightColoredImg[i]))
 
-def improvedAgent():
+    # Show final image
+    plt.imshow(new)
+    plt.show()
