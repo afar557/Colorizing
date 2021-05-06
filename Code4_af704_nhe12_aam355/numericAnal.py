06b5cf93d10a36b0bf3c366c639185a4ecb2cd38
Asma Makhdoom <asmamakhdoom@Asmas-MBP.lan>
2021-05-05 23:38:31 -0400

    numerical anal for improved

--- Code4_af704_nhe12_aam355/numericAnal.py
+++ Code4_af704_nhe12_aam355/numericAnal.py
@@ -1,12 +1,14 @@
 from basic import basicAgent
+from improved import improvedAgent
 import numpy as np
 
 def euclidDist(a , b):
     dist = np.linalg.norm(a-b)
     return(dist)
 
-def getAvgDist(imageName):
-    new, original = basicAgent(imageName)
+def getAvgDist(image):
+    # new, original = basicAgent(image)
+    new, original = improvedAgent(image)
     avg = 0
     for i in range(len(new)):
         for j in range(len(new[0])):
@@ -14,5 +16,3 @@ def getAvgDist(imageName):
     avg /= (len(new)*len(new[0]))
     # return euclidDist(new, original)
     return avg
-
-print(getAvgDist("OgPic.jpg"))
